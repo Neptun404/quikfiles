723798038ae73551fb19fcc64e2b35f91123caf9
Neptun404 <shadowova329@gmail.com>
2024-07-17 08:02:48 +0800

    sends error message if buffered file is too large

--- src/routes/routes-file.ts
+++ src/routes/routes-file.ts
@@ -1,12 +1,33 @@
+import { MultipartFields, MultipartFile, MultipartValue } from "@fastify/multipart";
 import { FastifyPluginAsync } from "fastify";
+// import fs from 'fs/promises';
 
 
-
+import fs from 'fs';
+import util from 'util';
+import path from 'path';
+import { pipeline } from 'stream';
+const pump = util.promisify(pipeline)
 
 
 const fileRoutes: FastifyPluginAsync = async (fast, opts) => {
     fast.post('/upload', async (req, repl) => {
-        return "File upload"
+
+        // TODO Implement the required functionality to upload files into s3
+        // const { name, file } = req.body as { name: MultipartValue, file: MultipartFile };
+
+        // Gets user uploaded file
+        const data = await req.file()
+        if (data === undefined) return;
+
+        // Buffer file to memory
+        const buffer = await bufferFile(data)
+        if (buffer.truncated || buffer.buffer === null) {
+            repl.statusCode = 413
+            return `${data.filename} is too large`
+        }
+
+        return `${data?.filename} uploaded`;
     })
 
     fast.get('/download/:id', async (req, repl) => {
@@ -14,6 +35,15 @@ const fileRoutes: FastifyPluginAsync = async (fast, opts) => {
 
         return `Downloading file with id ${id}`
     })
+}
+
+
+async function bufferFile(multipartFile: MultipartFile): Promise<{ truncated: boolean, buffer: Buffer | null }> {
+    const buffer = await multipartFile.toBuffer().catch(_ => { })
+    return {
+        truncated: multipartFile.file.truncated,
+        buffer: buffer === undefined ? null : buffer
+    }
 }
 
 export default fileRoutes
