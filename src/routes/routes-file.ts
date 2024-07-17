6d80369d746cf43b271fad25406760f7a6cf6ae0
Neptun404 <shadowova329@gmail.com>
2024-07-17 12:24:17 +0800

    Implemented s3 uploads and database insert

--- src/routes/routes-file.ts
+++ src/routes/routes-file.ts
@@ -5,10 +5,12 @@ import { FastifyPluginAsync } from "fastify";
 
 import fs from 'fs';
 import util from 'util';
-import path from 'path';
+import path, { join } from 'path';
 import { pipeline } from 'stream';
 const pump = util.promisify(pipeline)
 import { PrismaClient } from "@prisma/client";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import shortUUID from "short-uuid";
 
 const fileRoutes: FastifyPluginAsync = async (fast, opts) => {
     fast.post('/upload', async (req, repl) => {
@@ -23,20 +25,51 @@ const fileRoutes: FastifyPluginAsync = async (fast, opts) => {
             return `${data.filename} is too large`
         }
 
-        // TODO Save file into s3
 
+        // Get file extention
+        const { filename, fileExtension } = (() => {
+            const separatedStr = data.filename.split('.')
+            return {
+                fileExtension: separatedStr.pop(),
+                filename: data.filename
+            }
+        })()
+
+        // Generate an s3 object key with the file extention
+        const objectKey = shortUUID.generate().toLocaleLowerCase()
+
+        // TODO Save file into s3
+        const s3client = new S3Client({
+            region: process.env.REGION!,
+            credentials: {
+                secretAccessKey: process!.env.ACCESS_KEY!,
+                accessKeyId: process!.env.ACCESS_KEY_ID!
+            }
+        })
+        const response = await s3client.send(new PutObjectCommand({
+            Key: objectKey,
+            Bucket: 'quikfilesbucket',
+            Body: buffer.buffer
+        }))
+
+        // Build s3 uri link
+        const objectURI = `https://quikfilesbucket.s3.ap-southeast-1.amazonaws.com/${objectKey}.${fileExtension}`
 
         // Save file link into database
         const prisma = new PrismaClient()
         const fileLink = await prisma.fileLink.create({
             data: {
-                link: data.filename
+                uri: objectURI,
+                name: filename,
+                id: objectKey
             }
         }).catch(e => console.error(e))
 
+        console.log(fileLink);
+
         return {
-            message: `${data?.filename} uploaded`,
-            fileID: fileLink!.id
+            message: `${filename} uploaded`,
+            fileID: objectKey
         }
     })
 
